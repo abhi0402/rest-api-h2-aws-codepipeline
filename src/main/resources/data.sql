25e8b5a3f33e5dc1e3e342f2bbc1a71b2f5684ed
Abhishek Yadav <abhishekyadav153@gmail.com>
2019-12-18 17:25:59 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'abhi', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'abhi', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'abhi', 'Learn Microservices', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10004, 'david', 'Learn to drive a car', sysdate(), false);
